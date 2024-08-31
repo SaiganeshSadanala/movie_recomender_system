81c9769ddfc89dfd64cb4bb78363ffed80df76f6
Saiganesh Sadanala <127324464+SaiganeshSadanala@users.noreply.github.com>
2024-08-31 12:46:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,12 @@ st.set_page_config(
     initial_sidebar_state="collapsed",
 )
 
+# Initialize session state variables if not already initialized
+if 'recommend' not in st.session_state:
+    st.session_state.recommend = ([], [], [], [])
+if 'selected_movie' not in st.session_state:
+    st.session_state.selected_movie = None
+
 def fetch_poster(movie_id):
     url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key={API_KEY}"
     try:
@@ -117,6 +123,23 @@ st.markdown("""
         position: relative;
         z-index: 1;
     }
+
+    /* Video container */
+    .video-container {
+        position: relative;
+        padding-bottom: 56.25%; /* 16:9 aspect ratio */
+        height: 0;
+        overflow: hidden;
+        max-width: 100%;
+        background: #000;
+    }
+    .video-container iframe {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+    }
     </style>
     """, unsafe_allow_html=True)
 
@@ -138,8 +161,11 @@ similarity = pickle.load(open('similarity.pkl', 'rb'))
 # Define a callback function for when a movie is selected
 def movie_selected():
     st.session_state.selected_movie = st.session_state['selected_movie']
-    if 'recommendations' not in st.session_state or st.session_state.selected_movie != st.session_state.recommendations[0]:
-        st.session_state.recommendations = recommend(st.session_state.selected_movie)
+    if st.session_state.selected_movie:
+        if 'recommendations' not in st.session_state or st.session_state.selected_movie != st.session_state.recommend[0]:
+            st.session_state.recommend = recommend(st.session_state.selected_movie)
+    else:
+        st.session_state.recommend = ([], [], [], [])
     # Add JavaScript to blur the input field after a selection
     st.markdown("""
         <script>
@@ -149,62 +175,75 @@ def movie_selected():
 
 selected_movie = st.selectbox(
     "Type or select a movie from the dropdown",
-    movies['title'].values,
+    options=['Select any movie'] + list(movies['title'].values),
     key='selected_movie',
     on_change=movie_selected
 )
 
 def embed_youtube_video(video_id):
-    return f'<div class="video-container"><iframe src="https://www.youtube.com/embed/{video_id}?autoplay=1" frameborder="0" allowfullscreen></iframe></div>'
+    return f'''
+    <div class="video-container">
+        <iframe src="https://www.youtube.com/embed/{video_id}" 
+                frameborder="0" 
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                allowfullscreen>
+        </iframe>
+    </div>
+    '''
 
-st.markdown(f"<h2 class='fade-in'>Selected Movie: {selected_movie}</h2>", unsafe_allow_html=True)
-col1, col2, col3 = st.columns([2, 1, 2])
+if st.session_state.selected_movie:
+    st.markdown(f"<h2 class='fade-in'>Selected Movie: {st.session_state.selected_movie}</h2>", unsafe_allow_html=True)
+    col1, col2, col3 = st.columns([2, 1, 2])
 
-selected_movie_index = movies[movies['title'] == selected_movie].index[0]
-selected_movie_data = movies.iloc[selected_movie_index]
-selected_movie_poster = fetch_poster(selected_movie_data.id)
+    selected_movie_index = movies[movies['title'] == st.session_state.selected_movie].index[0]
+    selected_movie_data = movies.iloc[selected_movie_index]
+    selected_movie_poster = fetch_poster(selected_movie_data.id)
 
-# Embed image with custom class for selected movie poster
-col1.markdown(f"<img src='{selected_movie_poster}' class='selected-poster-img'>", unsafe_allow_html=True)
+    # Embed image with custom class for selected movie poster
+    col1.markdown(f"<img src='{selected_movie_poster}' class='selected-poster-img'>", unsafe_allow_html=True)
 
-middle_col = col2.empty()
+    middle_col = col2.empty()
 
-with middle_col:
-    st.markdown(f"**Cast**: {selected_movie_data.cast}", unsafe_allow_html=True)
-with col3:
-    trailer_link = fetch_trailer(selected_movie_data.id)
-    if trailer_link:
-        st.markdown(embed_youtube_video(trailer_link.split('=')[-1]), unsafe_allow_html=True)
+    with middle_col:
+        st.markdown(f"**Cast**: {selected_movie_data.cast}", unsafe_allow_html=True)
+    with col3:
+        trailer_link = fetch_trailer(selected_movie_data.id)
+        if trailer_link:
+            video_id = trailer_link.split('=')[-1]
+            st.markdown(embed_youtube_video(video_id), unsafe_allow_html=True)
 
-with col2:
-    movie_expander = st.expander(label=f"Details of {selected_movie}", expanded=True)
-    with movie_expander:
-        st.markdown(f"<div class='slide-down'>{selected_movie_data.overview}</div>", unsafe_allow_html=True)
-
-# Fetch and display providers with icons and links in a single row
-top_country, top_country_providers = fetch_providers(selected_movie_data.id)
-if top_country and top_country_providers:
-    st.markdown(f"**Available in {top_country} on:**", unsafe_allow_html=True)
-    provider_cols = st.columns(len(top_country_providers))
-    for idx, provider in enumerate(top_country_providers):
-        provider_name = provider['provider_name']
-        provider_icon = f"https://image.tmdb.org/t/p/original{provider['logo_path']}"
-        provider_url = provider.get('link', "#")
-        with provider_cols[idx]:
-            st.markdown(f"<a href='{provider_url}' target='_blank'><img src='{provider_icon}' alt='{provider_name}' title='{provider_name}' width='50'></a> {provider_name}", unsafe_allow_html=True)
-
-st.markdown(f"**Recommendations for movie:** {selected_movie}", unsafe_allow_html=True)
-col1, col2, col3, col4, col5, col6 = st.columns(6)
-
-names, posters, info, cast = st.session_state.recommendations
-
-for i in range(6):
-    with col1 if i == 0 else col2 if i == 1 else col3 if i == 2 else col4 if i == 3 else col5 if i == 4 else col6:
-        st.markdown(f"<b class='fade-in'>{names[i]}</b>", unsafe_allow_html=True)
-        st.markdown(f"<img src='{posters[i]}' class='poster-img'>", unsafe_allow_html=True)
-        movie_expander = st.expander(label=f"Details for {names[i]}", expanded=False)
+    with col2:
+        movie_expander = st.expander(label=f"Details of {st.session_state.selected_movie}", expanded=True)
         with movie_expander:
-            st.markdown(f"<div class='slide-down'>{info[i]}</div>", unsafe_allow_html=True)
+            st.markdown(f"<div class='slide-down'>{selected_movie_data.overview}</div>", unsafe_allow_html=True)
+
+    # Fetch and display providers with icons and links in a single row
+    top_country, top_country_providers = fetch_providers(selected_movie_data.id)
+    if top_country and top_country_providers:
+        st.markdown(f"**Available in {top_country} on:**", unsafe_allow_html=True)
+        provider_cols = st.columns(len(top_country_providers))
+        for idx, provider in enumerate(top_country_providers):
+            provider_name = provider['provider_name']
+            provider_icon = f"https://image.tmdb.org/t/p/original{provider['logo_path']}"
+            provider_url = provider.get('link', "#")
+            with provider_cols[idx]:
+                st.markdown(f"<a href='{provider_url}' target='_blank'><img src='{provider_icon}' alt='{provider_name}' title='{provider_name}' width='50'></a> {provider_name}", unsafe_allow_html=True)
+
+    st.markdown(f"**Recommendations for movie:** {st.session_state.selected_movie}", unsafe_allow_html=True)
+    col1, col2, col3, col4, col5, col6 = st.columns(6)
+
+    names, posters, info, cast = st.session_state.recommend
+
+    for i in range(6):
+        with col1 if i == 0 else col2 if i == 1 else col3 if i == 2 else col4 if i == 3 else col5 if i == 4 else col6:
+            st.markdown(f"<b class='fade-in'>{names[i]}</b>", unsafe_allow_html=True)
+            st.markdown(f"<img src='{posters[i]}' class='poster-img'>", unsafe_allow_html=True)
+            movie_expander = st.expander(label=f"Details for {names[i]}", expanded=False)
+            with movie_expander:
+                st.markdown(f"<div class='slide-down'>{info[i]}</div>", unsafe_allow_html=True)
+
+else:
+    st.markdown("<h2>Select any movie</h2>", unsafe_allow_html=True)
 
 # Close the content div
 st.markdown("</div>", unsafe_allow_html=True)
